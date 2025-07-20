be200e6c15bac714d87f1e13e4ac9c1adf287cb8
maxh2118 <maxhendriks@gmail.com>
2025-07-20 23:13:35 +0200

    Final fix: Simplify build and deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+// Dit is de meest simpele, kale configuratie.
 export default defineConfig({
-  // Haal de configuratie uit de 'environment variables'
-  site: process.env.PUBLIC_SITE_URL,
-  base: process.env.PUBLIC_BASE_URL,
-
   integrations: [tailwind(), react()]
 });
