dc32fbdf7c4edecbbc7a5fa62eb8704e67406555
maxh2118 <maxhendriks@gmail.com>
2025-07-20 23:25:54 +0200

    Final deploy: Use standard gh-pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import react from "@reactjs/react";
 
 export default defineConfig({
-  // De base is weer nodig voor deze methode
+  // Deze base is BELANGRIJK
   base: '/ninja-creami-site/',
   
   integrations: [tailwind(), react()]
