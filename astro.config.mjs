28849ac7906d2ea1c3cdf9608aaa634e09adff3d
maxh2118 <maxhendriks@gmail.com>
2025-07-20 23:27:31 +0200

    Fix: Correct typo in astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@reactjs/react";
+import react from "@astrojs/react"; // <-- Dit is de correctie
 
 export default defineConfig({
   // Deze base is BELANGRIJK
   base: '/ninja-creami-site/',
-  
+
   integrations: [tailwind(), react()]
 });
