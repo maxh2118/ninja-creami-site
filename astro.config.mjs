1331ee2d472558328a2ac2d903171420d5fe2b91
maxh2118 <maxhendriks@gmail.com>
2025-07-20 23:18:56 +0200

    Switch to manual subtree deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-// Dit is de meest simpele, kale configuratie.
 export default defineConfig({
+  // De base is weer nodig voor deze methode
+  base: '/ninja-creami-site/',
+  
   integrations: [tailwind(), react()]
 });
